9fb0f60726a2c0cbcd1742cf06c7dd1019add682
Caleb Siegel <caleb.siegel@gmail.com>
2024-01-09 15:29:24 -0500

    Copleted lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun (satActivity = "roller-skate") {
+    return `This Saturday, I want to ${satActivity}!`
+};
+
+function mondayWork (monActivity = "go to the office") {
+    return `This Monday, I will ${monActivity}.`
+};
+
+function wrapAdjective (prompt = "*") {
+    return function (adjective = "special") {
+        return `You are ${prompt}${adjective}${prompt}!`
+    }
+}
